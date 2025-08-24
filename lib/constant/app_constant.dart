8ccc6ce7bb43cbae2e1ab55bcd7e5125a8d58778
Pong1111 <153060807+Pong1111@users.noreply.github.com>
2025-08-25 02:50:16 +0700

    Add achievements screen and update navigation

---
+++ lib/constant/app_constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Colors.blue;
+const Color secondaryColor = Colors.grey;
+
+TextStyle headingtextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle bodyTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+);
+
